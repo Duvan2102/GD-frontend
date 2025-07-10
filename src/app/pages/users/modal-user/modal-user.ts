943983c310180c72ed5fa329f5c706df9acb4e52
Lucho <lg-pc11@hotmail.com>
2025-07-09 22:29:38 -0500

    edición de archivos sidebar.css adicional creación de componentes modal y modal-user

---
+++ src/app/pages/users/modal-user/modal-user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-user',
+  imports: [],
+  templateUrl: './modal-user.html',
+  styleUrl: './modal-user.css'
+})
+export class ModalUser {
+
+}
