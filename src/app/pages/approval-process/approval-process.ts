7d998e47ce8b14e8528ac9638cab1f5ac47ac9fa
LuisPerez96 <lg-pc11@hotmail.com>
2026-01-09 07:14:48 -0500

    Correcciòn de estados en approval-process

--- src/app/pages/approval-process/approval-process.ts
+++ src/app/pages/approval-process/approval-process.ts
@@ -27,7 +27,7 @@ export interface Approval {
   creatorFullName: string;
   position: string;
   lastUpdate: string;
-  status: 'PENDIENTE' | 'APROBADO' | 'RECHAZADO' | 'CANCELADA' | 'APROB-PENDIENTE';
+  status: 'PENDIENTE' | 'APROBADO' | 'RECHAZADO' | 'CANCELADA' | 'APROB-PENDIENTE' | 'APROB-POCESADO';
   approvers: { initials: string; fullName: string }[];
   priority: boolean;
   fullData?: any;
